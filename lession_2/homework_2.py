6234f8ea5b9e7ddeb5f40966b96d903f03f42143
AndrewKarpov <prorok.karpov@mail.ru>
2026-08-13 00:44:12 +0300

    )

---
+++ lession_2/homework_2.py
@@ -0,0 +1,102 @@
+#1
+print("Задание 1")
+a = 8
+b = 4
+x = 3.4
+y = 3.1
+print(type(a), type(b), type(x), type(y))
+
+#2
+print("Задание 2")
+num1 = 2
+num2 = 5
+print(num1 + num2)
+print(num1 - num2)
+print(num1 / num2)
+print(num1 // num2)
+print(num1 % num2)
+print(num1 ** num2)
+
+#3
+print("Задание 3")
+a = 10
+b = 3
+print(a / b)
+print(a // b)
+print(a % b)
+a = -10
+b = -3
+print(a / b)
+print(a // b)
+print(a % b)
+
+#4
+print("Задание 4")
+print(5 + 3 * 2)
+print((5 + 3) * 2)
+print(10 / 2 ** 2)
+
+#5
+print("Задание 5")
+count = 10
+count += 5
+count -= 3
+count *= 2
+count /= 4
+print(count)
+
+#Строки в Python
+#1
+print("Строки в Python \n Задание 1")
+s1 = "Python"
+s2 = 'Программирование'
+print(s1 + " " + s2)
+empty = ""
+print(len(empty))
+
+#2
+print("Задание 2")
+first_name = "Иван"
+last_name = "Петров"
+full_name = first_name + " " + last_name
+print(full_name)
+
+#3
+print("Задание 3")
+s = "Age"
+age = 5
+print(s + " " + str(age))
+
+#4
+print("Задание 4")
+print("xa" * 5)
+
+#5
+print("Задание 5")
+text = "Привет, мир!"
+print(len(text))
+text0 = ""
+print(len(text0))
+
+#6
+print("Задание 6")
+sentence = "Я изучаю Python"
+print("Python" in sentence)
+print(sentence in "Java")
+
+#7
+print("Задание 7")
+a = "apple"
+b = "banana"
+print(a == b)
+print(a != b)
+print(a < b)
+print(a > b)
+print(a <= b)
+print(a >= b)
+
+#8
+print("Задание 8")
+print(ord('A'))
+print(ord('a'))
+print(ord('Я'))
